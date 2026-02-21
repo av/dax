e35cd9de6c102581bf8e8586089df0e4be2d0e98
Ivan Charapanau <mail@av.codes>
2026-02-21 11:09:24 +0100

    chore: cleanup

--- src/ui/stores/index.ts
+++
@@ -1,4 +0,0 @@
-export { useSceneStore } from './sceneStore';
-export { useWorkspaceStore } from './workspaceStore';
-export { useAgentStore } from './agentStore';
-export { useSettingsStore } from './settingsStore';
